b67040566da544ce8c7bf920994f1ddaceb18a0b
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-12-14 22:19:54 -0600

    Add social tags for IOTA Fountain. Generalize config.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,28 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const bitcoinAppConfig = {
+  VUE_APP_TITLE: "Bitcoin Fountain",
+  VUE_APP_URL: "https://www.bitcoin-fountain.com",
+  VUE_APP_SOCIAL_IMAGE: "https://www.bitcoin-fountain.com/iota-fountain-reduced.png",
+  VUE_APP_DESCRIPTION: "Watch transactions flow on the Bitcoin network"
+}
+
+const iotaAppConfig = {
+  VUE_APP_TITLE: "IOTA Fountain",
+  VUE_APP_URL: "https://www.iota-fountain.com",
+  VUE_APP_SOCIAL_IMAGE: "https://www.iota-fountain.com/iota-fountain-reduced.png",
+  VUE_APP_DESCRIPTION: "Watch transactions flow on the IOTA network"
+}
+
+let activeConfig = iotaAppConfig
+
+if(process.env.VUE_APP_BITCOIN_FOUNTAIN) {
+  activeConfig = bitcoinAppConfig
+}
+
+process.env = {...process.env, ...activeConfig}
+
 module.exports = {
   configureWebpack: {
     plugins: [
