3d0a38b7f35b34bfdec53e883712e1ce7a9d4060
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2020-01-03 12:05:03 -0600

    Set social config for NANO Fountain

--- vue.config.js
+++ vue.config.js
@@ -2,25 +2,54 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const bitcoinAppConfig = {
-  VUE_APP_TITLE: "Bitcoin Fountain",
-  VUE_APP_URL: "https://www.bitcoin-fountain.com",
-  VUE_APP_SOCIAL_IMAGE: "https://www.bitcoin-fountain.com/iota-fountain-reduced.png",
-  VUE_APP_DESCRIPTION: "Watch transactions flow on the Bitcoin network"
+const coinFlags = new Set([
+  "VUE_APP_BITCOIN_FOUNTAIN",
+  "VUE_APP_NANO_FOUNTAIN",
+  "VUE_APP_IOTA_FOUNTAIN"
+])
+
+function getCurrentCoinKey() {
+  let existingCoinFlags = new Set()
+
+  for(let coinFlag of coinFlags) {
+    if(process.env[coinFlag]) {
+      existingCoinFlags.add(coinFlag)
+    }
+  }
+
+  if(existingCoinFlags.size !== 1) {
+    throw `Must supply exactly one of ${JSON.stringify(coinFlags)}, in the environment`
+  }
+
+  return existingCoinFlags.values().next().value
 }
 
-const iotaAppConfig = {
-  VUE_APP_TITLE: "IOTA Fountain",
-  VUE_APP_URL: "https://www.iota-fountain.com",
-  VUE_APP_SOCIAL_IMAGE: "https://www.iota-fountain.com/iota-fountain-reduced.png",
-  VUE_APP_DESCRIPTION: "Watch transactions flow on the IOTA network"
+const socialConfigs = {
+  VUE_APP_BITCOIN_FOUNTAIN: {
+    VUE_APP_TITLE: "Bitcoin Fountain",
+    VUE_APP_URL: "https://www.bitcoin-fountain.com",
+    VUE_APP_SOCIAL_IMAGE: "https://www.bitcoin-fountain.com/bitcoin-fountain-reduced.png",
+    VUE_APP_DESCRIPTION: "Watch transactions flow on the Bitcoin network"
+  },
+  VUE_APP_IOTA_FOUNTAIN: {
+    VUE_APP_TITLE: "IOTA Fountain",
+    VUE_APP_URL: "https://www.iota-fountain.com",
+    VUE_APP_SOCIAL_IMAGE: "https://www.iota-fountain.com/iota-fountain-reduced.png",
+    VUE_APP_DESCRIPTION: "Watch transactions flow on the IOTA network"
+  },
+  VUE_APP_NANO_FOUNTAIN: {
+    VUE_APP_TITLE: "NANO Fountain",
+    VUE_APP_URL: "https://www.nano-fountain.com",
+    VUE_APP_SOCIAL_IMAGE: "https://www.nano-fountain.com/nano-fountain-reduced.png",
+    VUE_APP_DESCRIPTION: "Watch transactions flow on the NANO network"
+  }
 }
 
-let activeConfig = iotaAppConfig
-
-if(process.env.VUE_APP_BITCOIN_FOUNTAIN) {
-  activeConfig = bitcoinAppConfig
+function getActiveSocialConfig() {
+  return socialConfigs[getCurrentCoinKey()]
 }
+
+const activeConfig = getActiveSocialConfig()
 
 process.env = {...process.env, ...activeConfig}
 
